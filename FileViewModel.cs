89ffbb7ff31a7586e4825cadf03f316ca6ca6ca3
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2025-08-20 11:45:31 -0600

    Agregar archivos de proyecto.

---
+++ FileViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackIt
+{
+    public class FileViewModel : INotifyPropertyChanged
+    {
+
+        private string _name;
+        private string _type;
+
+        public string? Name { 
+            get { return _name; } 
+            set { 
+                this._name = value!;
+                OnPropertyChanged();
+            } 
+        }
+        public string? Type
+        {
+            get { return _type; }
+            set
+            {
+                this._type = value!;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //private FileType _file;
+
+        public FileViewModel()
+        {
+            //_file = new FileType();
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+    public class FileType
+    {
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.IO.Compression;
+using System.Windows;
+
+namespace PackIt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public FileViewModel fileViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            fileViewModel = (FileViewModel)this.DataContext;
+        }
+
+        private void LoadFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Multiselect = true;
+
+            if (!(bool)openFileDialog.ShowDialog()!)
+            {
+                return;
+            }
+
+            string[] values = openFileDialog.SafeFileName.Split('.');
+
+            if (!(values.Length >= 2))
+            {
+                return;
+            }
+
+            fileViewModel.Name = $"Name: {values[0]}";
+            fileViewModel.Type = $"Type: {values[values.Length - 1]}";
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "ZIP Folders (.ZIP)|*.zip";
+            saveFileDialog.FileName = "data";
+
+            if ((bool)saveFileDialog.ShowDialog()!)
+            {
+                ZipOneFile(stream: openFileDialog.OpenFile(), pathSave: saveFileDialog.FileName, fileName: openFileDialog.SafeFileName);
+            }
+
+        }
+
+        private void ZipOneFile(Stream stream, string pathSave, string fileName)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+
+            using (var zip = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+            {
+                var entry = zip.CreateEntry(fileName, CompressionLevel.Optimal);
+                using (var streamEntru = entry.Open())
+                {
+                    stream.Position = 0;
+                    stream.CopyTo(streamEntru);
+                }
+            }
+
+            File.WriteAllBytes(pathSave, memoryStream.ToArray());
+
+            memoryStream.Dispose();
+            memoryStream.Close();
+            //return memoryStream.ToArray();
+        }
+    }
+}
